03e9514f221817fbf826937f96f69988787ec307
Julen Urizar Compains <julenuri@hotmail.com>
2026-04-02 14:05:39 +0200

    [7z] Fix system

--- CPP/Windows/System.cpp
+++ CPP/Windows/System.cpp
@@ -14,7 +14,7 @@ UInt32 GetNumberOfProcessors()
   return (UInt32)systemInfo.dwNumberOfProcessors;
 }
 
-#if !defined(_WIN64) && defined(__GNUC__)
+#if !defined(_WIN64) && (defined(__GNUC__) || (defined(_MSC_VER) && _MSC_VER < 1300))
 
 typedef struct _MY_MEMORYSTATUSEX {
   DWORD dwLength;

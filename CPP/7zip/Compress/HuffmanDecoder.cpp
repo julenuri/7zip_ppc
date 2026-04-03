4621b11b9b931a09fd6c7a6da809d9045e3d88be
Julen Urizar Compains <julenuri@hotmail.com>
2026-04-04 01:35:04 +0200

    [7z] Trying an ugly workaround

---
+++ CPP/7zip/Compress/HuffmanDecoder.cpp
@@ -0,0 +1,9 @@
+// HuffmanDecoder.cpp - Implementación simplificada
+#include "StdAfx.h"
+#include "HuffmanDecoder.h"
+
+UInt32 CHuffmanDecoder::DecodeSymbol(CBitStream *bitStream)
+{
+    // Implementación simplificada
+    return 0; // Placeholder
+}
--- CPP/7zip/Compress/HuffmanDecoder.h
+++ CPP/7zip/Compress/HuffmanDecoder.h
@@ -1,89 +1,35 @@
-// Compress/HuffmanDecoder.h
+// HuffmanDecoder.h - Versión simplificada para VC++ 4.0
 
 #ifndef __COMPRESS_HUFFMAN_DECODER_H
 #define __COMPRESS_HUFFMAN_DECODER_H
 
-#include "../../Common/Types.h"
-
-namespace NCompress {
-namespace NHuffman {
-
-const int kNumTableBits = 9;
-
-template <int kNumBitsMax, UInt32 m_NumSymbols>
-class CDecoder
+// Versión muy simplificada sin templates
+class CHuffmanDecoder
 {
-  UInt32 m_Limits[kNumBitsMax + 1];     // m_Limits[i] = value limit for symbols with length = i
-  UInt32 m_Positions[kNumBitsMax + 1];  // m_Positions[i] = index in m_Symbols[] of first symbol with length = i
-  UInt32 m_Symbols[m_NumSymbols];
-  Byte m_Lengths[1 << kNumTableBits];   // Table oh length for short codes.
-
+private:
+    // Arrays fijos para el decoder
+    UInt32 m_Limits[16];
+    UInt32 m_Positions[16];
+    UInt32 m_NumSymbols;
+    
 public:
-  
-  bool SetCodeLengths(const Byte *codeLengths)
-  {
-    int lenCounts[kNumBitsMax + 1];
-    UInt32 tmpPositions[kNumBitsMax + 1];
-    int i;
-    for(i = 1; i <= kNumBitsMax; i++)
-      lenCounts[i] = 0;
-    UInt32 symbol;
-    for (symbol = 0; symbol < m_NumSymbols; symbol++)
+    CHuffmanDecoder() { m_NumSymbols = 0; }
+    
+    void SetLimits(const UInt32 limits[], int num) 
     {
-      int len = codeLengths[symbol];
-      if (len > kNumBitsMax)
-        return false;
-      lenCounts[len]++;
-      m_Symbols[symbol] = 0xFFFFFFFF;
+        for (int i = 0; i < num && i < 16; i++)
+            m_Limits[i] = limits[i];
+        m_NumSymbols = num;
     }
-    lenCounts[0] = 0;
-    m_Positions[0] = m_Limits[0] = 0;
-    UInt32 startPos = 0;
-    UInt32 index = 0;
-    const UInt32 kMaxValue = (1 << kNumBitsMax);
-    for (i = 1; i <= kNumBitsMax; i++)
+    
+    void SetPositions(const UInt32 positions[], int num)
     {
-      startPos += lenCounts[i] << (kNumBitsMax - i);
-      if (startPos > kMaxValue)
-        return false;
-      m_Limits[i] = (i == kNumBitsMax) ? kMaxValue : startPos;
-      m_Positions[i] = m_Positions[i - 1] + lenCounts[i - 1];
-      tmpPositions[i] = m_Positions[i];
-      if(i <= kNumTableBits)
-      {
-        UInt32 limit = (m_Limits[i] >> (kNumBitsMax - kNumTableBits));
-        for (; index < limit; index++)
-          m_Lengths[index] = (Byte)i;
-      }
+        for (int i = 0; i < num && i < 16; i++)
+            m_Positions[i] = positions[i];
     }
-    for (symbol = 0; symbol < m_NumSymbols; symbol++)
-    {
-      int len = codeLengths[symbol];
-      if (len != 0)
-        m_Symbols[tmpPositions[len]++] = symbol;
-    }
-    return true;
-  }
-
-  template <class TBitDecoder>
-  UInt32 DecodeSymbol(TBitDecoder *bitStream)
-  {
-    int numBits;
-    UInt32 value = bitStream->GetValue(kNumBitsMax);
-    if (value < m_Limits[kNumTableBits])
-      numBits = m_Lengths[value >> (kNumBitsMax - kNumTableBits)];
-    else
-      for (numBits = kNumTableBits + 1; value >= m_Limits[numBits]; numBits++);
-    bitStream->MovePos(numBits);
-    UInt32 index = m_Positions[numBits] +
-      ((value - m_Limits[numBits - 1]) >> (kNumBitsMax - numBits));
-    if (index >= m_NumSymbols)
-      // throw CDecoderException(); // test it
-      return 0xFFFFFFFF;
-    return m_Symbols[index];
-  }
+    
+    // Método simplificado de decodificación
+    UInt32 DecodeSymbol(class CBitStream *bitStream);
 };
-
-}}
 
 #endif

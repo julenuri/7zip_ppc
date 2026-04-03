a77b7b5b8b9c45cbdfee60ac5c75336b8fdf82cd
Julen Urizar Compains <julenuri@hotmail.com>
2026-04-04 01:06:55 +0200

    [7z] A big, big mistake - No template, no party

--- DeshacerSoloLectura.bat
+++ DeshacerSoloLectura.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Borrando carpeta de PPCRel
-rmdir "C:\WINNT\Profiles\Administrator\Desktop\7zip_ppc\CPP\7zip\Bundles\Alone\PPCRel" /s
+rmdir "C:\WINNT\Profiles\Administrator\Desktop\7zip_ppc\CPP\7zip\Bundles\Alone\PPCRel" /s /q
 echo Copiando archivos...
 xcopy "G:\7zip_ppc\*" "C:\WINNT\Profiles\Administrator\Desktop\7zip_ppc" /E /I
 echo Copia completada.
